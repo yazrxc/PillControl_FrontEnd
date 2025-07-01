a8e49d4a0dfae286710897c092ab4cef4f4d9833
Kevinski2004 <kevinayquipa5@gmail.com>
2025-07-01 07:50:18 -0500

    Implementacion de Paginación para las tablas perfil y contacto

--- src/app/components/contactoemergencia/listarcontactoemergencia/listarcontactoemergencia.component.ts
+++ src/app/components/contactoemergencia/listarcontactoemergencia/listarcontactoemergencia.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
 import { ContactoEmergencia } from '../../../models/contactoemergencia';
 import { ContactoemergenciaService } from '../../../services/contactoemergencia.service';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listarcontactoemergencia',
@@ -21,8 +21,10 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './listarcontactoemergencia.component.html',
   styleUrl: './listarcontactoemergencia.component.css'
 })
-export class ListarcontactoemergenciaComponent implements OnInit {
+export class ListarcontactoemergenciaComponent implements OnInit , AfterViewInit{
   dataSource: MatTableDataSource<ContactoEmergencia> = new MatTableDataSource()
+  @ViewChild(MatPaginator) paginator!: MatPaginator;  // Correctamente asignamos el paginator
+  totalRegistros: number = 0;
 
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6','c7','c8']
 
@@ -31,15 +33,22 @@ export class ListarcontactoemergenciaComponent implements OnInit {
   ngOnInit(): void {
     this.cS.list().subscribe(data => {
       this.dataSource = new MatTableDataSource(data)
+      this.totalRegistros = data.length;
+      this.dataSource.paginator = this.paginator;
     });
     this.cS.getList().subscribe(data => {
       this.dataSource = new MatTableDataSource(data)
     })
   }
+  ngAfterViewInit(): void {
+  this.dataSource.paginator = this.paginator;
+  }
   eliminar(id:number){
     this.cS.deleteA(id).subscribe(data=>{
       this.cS.list().subscribe(data=>{
         this.cS.setList(data)
+        this.totalRegistros = data.length;
+        this.dataSource.paginator = this.paginator;
       })
     })
   }
--- src/app/components/perfilsalud/listarperfilsalud/listarperfilsalud.component.ts
+++ src/app/components/perfilsalud/listarperfilsalud/listarperfilsalud.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit,ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
 import { PerfilSalud } from '../../../models/perfilsalud';
 import { PerfilsaludService } from '../../../services/perfilsalud.service';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listarperfilsalud',
@@ -21,25 +21,34 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './listarperfilsalud.component.html',
   styleUrl: './listarperfilsalud.component.css'
 })
-export class ListarperfilsaludComponent implements OnInit {
+export class ListarperfilsaludComponent implements OnInit, AfterViewInit{
   dataSource: MatTableDataSource<PerfilSalud> = new MatTableDataSource()
+  @ViewChild(MatPaginator) paginator!: MatPaginator;  // Correctamente asignamos el paginator
 
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6','c7','c8','c9']
+  totalRegistros: number = 0;
 
   constructor(private pS: PerfilsaludService) { }
 
   ngOnInit(): void {
     this.pS.list().subscribe(data => {
       this.dataSource = new MatTableDataSource(data)
+      this.totalRegistros = data.length;
+      this.dataSource.paginator = this.paginator;
     });
     this.pS.getList().subscribe(data => {
       this.dataSource = new MatTableDataSource(data)
     })
   }
+  ngAfterViewInit(): void {
+  this.dataSource.paginator = this.paginator;
+}
   eliminar(id:number){
     this.pS.deleteA(id).subscribe(data=>{
       this.pS.list().subscribe(data=>{
         this.pS.setList(data)
+        this.totalRegistros = data.length;
+        this.dataSource.paginator = this.paginator;
       })
     })
   }
