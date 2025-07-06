d4930a990ff4f27ef2730741f93374db9b33d36e
Yazmin Chozo <u20221b313@upc.edu.pe>
2025-07-06 16:40:25 -0500

    actualizacion app ts para menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, RouterOutlet],
+  imports: [MenuComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PillControl_FrontEnd';
+  mostrarMenuFlag = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.mostrarMenuFlag = event.url !== '/landing';
+    });
+  }
 }
